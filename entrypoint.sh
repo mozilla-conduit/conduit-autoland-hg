ff9a1b4910ed6ea81a72fc2f409817558d76c176
Byron Jones <byron@glob.com.au>
2018-01-17 22:39:29 +0800

    $REPO --> $REPO_NAME

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 set -e
-REPO=${REOP:-test-repo}
+REPO_NAME=${REPO_NAME:-test-repo}
 case "${1:-start}" in
     "init")
-        if [ -d /repos/$REPO ]; then
+        if [ -d /repos/$REPO_NAME ]; then
             exit
         fi
-        echo "Initialising $REPO"
+        echo "Initialising $REPO_NAME"
         mkdir -p /repos
-        hg init /repos/$REPO
-        cd /repos/$REPO
-        echo $REPO > README
+        hg init /repos/$REPO_NAME
+        cd /repos/$REPO_NAME
+        echo $REPO_NAME > README
         hg commit -A -m 'initial commit'
         ;;
     "start")
         echo Starting hg web server on port ${PORT:-8000}
-        cd /repos/$REPO
+        cd /repos/$REPO_NAME
         exec hg serve --port ${PORT:-8000} --accesslog /dev/null
         ;;
     *)
